070e6e912c52e18c3a6327cdfbd3cd8d712208ee
Lenn Hypolite <lenntriniman@hotmail.com>
2016-09-21 20:10:48 +0000

    Done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,20 +10,30 @@ class PostsController < ApplicationController
 	def new
 		@post = Post.new
 	end
-
-	def create
-	  @post = Post.new(params["post"])
+	# -- for require the params that gets passed in must contain a key called "post".
+	def create         				#-- the post key
+	  # @post = Post.new(params.require(:post).permit(:title, :description))
+		@post = Post.new(post_params)
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
+	# -- permit only allows for the specified attribute to altered and saved by the user. In this case only the title.
 	def update
+		# @post.update(params.require(:post).permit(:title))
 	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
+	  @post.update(post_params)
 	  redirect_to post_path(@post)
 	end
 
 	def edit
 	  @post = Post.find(params[:id])
 	end
+
+	private
+
+	def post_params
+	  params.require(:post).permit(:title, :description)
+	end
+
 end
