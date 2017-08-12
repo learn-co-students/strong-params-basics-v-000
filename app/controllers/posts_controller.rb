63fd11a05c1bf958b27f022ad857770c99bc713a
Zain <zain@halai.us>
2017-08-12 18:09:55 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,17 +12,17 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
-	def create
-	  @post = Post.new(post_params)
-	  @post.save
-	  redirect_to post_path(@post)
-	end
-
-	def update
-	  @post = Post.find(params[:id])
-	  @post.update(post_params)
-	  redirect_to post_path(@post)
-	end
+def create
+  @post = Post.new(post_params(:title, :description))
+  @post.save
+  redirect_to post_path(@post)
+end
+ 
+def update
+  @post = Post.find(params[:id])
+  @post.update(post_params(:title))
+  redirect_to post_path(@post)
+end
 
 	def edit
 	  @post = Post.find(params[:id])
@@ -30,8 +30,8 @@ class PostsController < ApplicationController
 
 	private
 
-	def post_params
-	  params.require(:post).permit(:title, :description)
+	def post_params(*args)
+	  params.require(:post).permit(*args)
 	end
 
 end
