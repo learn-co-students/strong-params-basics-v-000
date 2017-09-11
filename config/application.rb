294a3acabd4d40b8c5e36b468561168d1498b885
hyoyou <heatherhyou@gmail.com>
2017-09-11 12:18:27 -0400

    Done.

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module BlogFlash
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.action_controller.permit_all_parameters = true
+    # Enable strong params
+    # config.action_controller.permit_all_parameters = true
   end
 end
