c8217b384643b93ca851cdc49c9d2cee8736a8c6
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-10-20 00:17:57 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,18 +12,36 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
+	  # @post = Post.new(params["post"]) # not secure
+		# @post = Post.new(params.require(:post).permit(:title, :description)) #too verbose
+		# @post = Post.new(post_params) #slightly inflexible version
+		@post = Post.new(post_params(:title, :description))
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
+	  # @post.update(params["post"]) #not secure
+		# @post.update(params.require(:post).permit(:title)) #too verbose
+		# @post.update(post_params)
+		@post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
 
 	def edit
 	  @post = Post.find(params[:id])
 	end
+
+	private
+
+	# def post_params # a bit inflexible, can't do, create two fields, only allow to fix 1.
+	# 	params.require(:post).permit(:title, :description)
+	# end
+
+	def post_params(*args) #pass in permitted fields as args.
+		params.require(:post).permit(*args)
+	end
+
+
 end
