bd0f0c744ab8a98deddb98d5afe10e8d48e840ca
goatcheese <nonsquishy@gmail.com>
2018-07-05 12:55:24 -0500

    Done.

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require File.expand_path("../boot", __FILE__)
 
 require "rails"
 # Pick the frameworks you want:
@@ -31,6 +31,5 @@ module BlogFlash
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.action_controller.permit_all_parameters = true
   end
 end
