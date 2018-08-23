580c64198cabff02663988c15db276a1937a0da1
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2018-08-23 10:50:04 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,20 +12,30 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  # @post = Post.new(params["post"])
-	  @post = Post.new(params.require(:post).permit(:title, :description))
+	  # @post = Post.new(params["post"]) - before stong params
+	  # @post = Post.new(params.require(:post).permit(:title, :description)) - with strong params 
+	  @post = Post.new(post_params) # DRY strong params
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
 	  @post = Post.find(params[:id])
-	  # @post.update(params["post"])
-	  @post.update(params.require(:post).permit(:title))
+	  # @post.update(params["post"]) - before stong params
+	  # @post.update(params.require(:post).permit(:title)) - with strong params 
+	  @post.update(post_params) # DRY strong params
 	  redirect_to post_path(@post)
 	end
 
 	def edit
 	  @post = Post.find(params[:id])
 	end
+
+	
+	private
+ 
+	def post_params
+  		params.require(:post).permit(:title, :description)
+	end
+
 end
