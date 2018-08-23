adc52f447da9634ac446928a5973a2e60b637b81
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2018-08-23 10:59:23 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
 	def create
 	  # @post = Post.new(params["post"]) - before stong params
 	  # @post = Post.new(params.require(:post).permit(:title, :description)) - with strong params 
-	  @post = Post.new(post_params) # DRY strong params
+	  @post = Post.new(post_params(:title, :description)) # DRY strong params using a private method below
 	  @post.save
 	  redirect_to post_path(@post)
 	end
@@ -23,7 +23,8 @@ class PostsController < ApplicationController
 	  @post = Post.find(params[:id])
 	  # @post.update(params["post"]) - before stong params
 	  # @post.update(params.require(:post).permit(:title)) - with strong params 
-	  @post.update(post_params) # DRY strong params
+	  @post.update(post_params(:title)) # DRY strong params using a private method below
+	  @post.save
 	  redirect_to post_path(@post)
 	end
 
@@ -31,11 +32,16 @@ class PostsController < ApplicationController
 	  @post = Post.find(params[:id])
 	end
 
+# Add a private method to DRY up strong params code to use in the create and update methods above
+# We pass the permitted fields in as *args;
+# this keeps `post_params` pretty dry while
+# still allowing slightly different behavior
+# depending on the controller action
 	
-	private
+	private 
  
-	def post_params
-  		params.require(:post).permit(:title, :description)
+	def post_params(*args) 
+  		params.require(:post).permit(*args)
 	end
 
 end
