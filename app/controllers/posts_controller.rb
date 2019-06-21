4fe494fbc9ac35399f99840d8b28589275a98a8e
github username <github email address>
2019-06-21 13:28:36 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,18 +12,29 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
+	  #@post = Post.new(params["post"])
+	  @post.update(post_params(:title,:description))
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
+	  @post.update(post_params(:title))
+	  #@post.update(params["post"])
 	  redirect_to post_path(@post)
 	end
 
 	def edit
 	  @post = Post.find(params[:id])
+	end
+
+	private #secures the helper method
+
+	def post_params(*args) #is the helper method 
+
+		params.require(:post).permit(*args)
+		#uses .require and .permit methods as stronger params to prevent hacks through inspect element right click edit html 
+		#require restricts, permit allows, and *args is for the custom arguments
 	end
 end
