f40e4f1001dad78e693904d76cd13b3a21de40ae
sdcro <sdcrouse91@gmail.com>
2019-11-20 20:01:38 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,18 +12,30 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
+	  @post = Post.new(post_params(:title, :description))
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
-	  redirect_to post_path(@post)
+	  @post.update(post_params(:title))
+    redirect_to post_path(@post)
+    
+    # Note: If I try to maliciously change the description here,
+    # it does nothing to the post, and it does NOT raise a ForbiddenAttributesError.
+    # This is true even if I submit a param with an invalid name (description instead of post[description]).
+    # However, as mentioned in the lesson, if I don't have a :post key in the params, 
+    # it fails with an error (ActionController::ParameterMissing, in this case).
 	end
 
 	def edit
 	  @post = Post.find(params[:id])
-	end
+  end
+  
+  private
+
+  def post_params(*args)
+    params.require(:post).permit(*args)
+  end
 end
